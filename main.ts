55085eb098c525a3ea82eb0d7bbe95e26c8cb638
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2025-08-05 14:15:34 -0700

    Clear Main

--- main.ts
+++ main.ts
@@ -1,7 +1,1 @@
-ai_recycle.onAgentRun(function () {
-    while (ai_recycle.recyclablesExist()) {
-        ai_recycle.goToItem()
-        ai_recycle.classifyItem()
-        ai_recycle.goToBin()
-    }
-})
+
